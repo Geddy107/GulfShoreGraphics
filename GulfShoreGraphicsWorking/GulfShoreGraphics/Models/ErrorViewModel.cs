8e59400378264c4da413427428823480612a2a12
John Geddeis <JohnGeddeis@outlook.com>
2019-10-28 14:13:09 -0400

    I have removed the "Project Step" prefix as the project has now evolved past the point of structured steps. GulfShoreGraphicsWorking is now the folder that is currently underconstruction. Color changes have been made to the Navbar.

---
+++ GulfShoreGraphicsWorking/GulfShoreGraphics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GulfShoreGraphics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
